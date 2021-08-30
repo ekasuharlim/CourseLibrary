a9399eb77ca101554e01e9342758a087c3b51ad8
eka <eka@example.com>
2021-08-31 07:58:58 +0800

    adding new resource

---
+++ CourseLibrary.Api/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("/api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository repo;
+
+        public AuthorsController(ICourseLibraryRepository repo) 
+        {
+            this.repo = repo;
+        }
+        [HttpGet]
+        public IActionResult GetAuthors() 
+        {
+            return  new JsonResult(this.repo.GetAuthors());
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthors(Guid authorId)
+        {
+            var author = this.repo.GetAuthors().FirstOrDefault(a => a.Id == authorId);
+            if (author != null)
+            {
+                return Ok(author);
+            }
+            else 
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
